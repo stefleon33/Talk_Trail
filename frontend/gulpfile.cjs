fd7ba510e238b348e96868835a73b006ce7abe78
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-06-05 14:32:35 -0400

    AutoPrefixer fix

--- frontend/gulpfile.cjs
+++ frontend/gulpfile.cjs
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
+const autoprefixer = require("gulp-autoprefixer").default;
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
 
