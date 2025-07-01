2521ade03817bcdb1fa4ce18d269c3941978602b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-07-01 16:50:00 -0400

    Initial Header

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+
+    return(
+        <header>
+            <div>
+                <div>
+                    <p>Trail Talk</p>
+                </div>
+                <div>
+                    <p>NavBar</p>
+                </div>
+            </div>
+        </header>
+    );
+}
